83036b3396dda77eafa6925e10c6d5400efe79fb
Salah-Eddine ElOuali <50548046+akasheidheda@users.noreply.github.com>
2023-01-22 15:11:26 +0100

    huh forget the case if number is already decimal

--- base converter/src/convert_base_file.c
+++ base converter/src/convert_base_file.c
@@ -1,28 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
 char* convert_base(char* num, int from_base, int to_base) {
     // error checking for invalid base
     if (from_base < 2 || from_base > 16 || to_base < 2 || to_base > 16) {
         return NULL;
     }
-
-    int decimal_num = 0, i, j, len = 0;
-    for (i = 0; num[i] != '\0'; i++) {
-        len++;
-    }
-    // convert from from_base to decimal
-    for (i = 0, j = len - 1; i < len; i++, j--) {
-        if (num[i] >= '0' && num[i] <= '9') {
-            decimal_num += (num[i] - '0') * pow(from_base, j);
-        } else if (num[i] >= 'A' && num[i] <= 'F') {
-            decimal_num += (num[i] - 'A' + 10) * pow(from_base, j);
-        } else {
-            return NULL;
+    int decimal_num, i, j, len = 0;
+    if (from_base == 10) {
+        // num is already in decimal, no need to convert
+        decimal_num = atoi(num);
+    } else {
+        for (i = 0; num[i] != '\0'; i++) {
+            len++;
+        }
+        // convert from from_base to decimal
+        for (i = 0, j = len - 1; i < len; i++, j--) {
+            if (num[i] >= '0' && num[i] <= '9') {
+                decimal_num += (num[i] - '0') * pow(from_base, j);
+            } else if (num[i] >= 'A' && num[i] <= 'F') {
+                decimal_num += (num[i] - 'A' + 10) * pow(from_base, j);
+            } else {
+                return NULL;
+            }
         }
     }
-
     // convert decimal to to_base
     int quotient = decimal_num, remainder, temp = 0;
     char* converted_num = (char*)malloc(sizeof(char) * len);
