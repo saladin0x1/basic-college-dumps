4e686fd3253fe1444ef9c120b4c50edf6096569e
Salah-Eddine ElOuali <50548046+akasheidheda@users.noreply.github.com>
2023-01-22 23:33:53 +0100

    Add files via upload

--- base converter/src/convert_base_file.c
+++ base converter/src/convert_base_file.c
@@ -3,44 +3,43 @@ char* convert_base(char* num, int from_base, int to_base) {
     if (from_base < 2 || from_base > 16 || to_base < 2 || to_base > 16) {
         return NULL;
     }
-    int decimal_num, i, j, len = 0;
-    if (from_base == 10) {
-        // num is already in decimal, no need to convert
-        decimal_num = atoi(num);
-    } else {
-        for (i = 0; num[i] != '\0'; i++) {
-            len++;
-        }
-        // convert from from_base to decimal
-        for (i = 0, j = len - 1; i < len; i++, j--) {
-            if (num[i] >= '0' && num[i] <= '9') {
-                decimal_num += (num[i] - '0') * pow(from_base, j);
-            } else if (num[i] >= 'A' && num[i] <= 'F') {
-                decimal_num += (num[i] - 'A' + 10) * pow(from_base, j);
-            } else {
-                return NULL;
-            }
+
+    // convert from_base to decimal
+    int decimal_num = 0;
+    int len = strlen(num);
+    for (int i = 0, j = len - 1; i < len; i++, j--) {
+        if (num[i] >= '0' && num[i] <= '9') {
+            decimal_num += (num[i] - '0') * pow(from_base, j);
+        } else if (num[i] >= 'A' && num[i] <= 'F') {
+            decimal_num += (num[i] - 'A' + 10) * pow(from_base, j);
+        } else {
+            return NULL;
         }
     }
+
     // convert decimal to to_base
-    int quotient = decimal_num, remainder, temp = 0;
     char* converted_num = (char*)malloc(sizeof(char) * len);
-    while (quotient != 0) {
-        remainder = quotient % to_base;
+    int temp = 0;
+    while (decimal_num != 0) {
+        int remainder = decimal_num % to_base;
         if (remainder < 10) {
             converted_num[temp++] = remainder + '0';
         } else {
             converted_num[temp++] = remainder - 10 + 'A';
         }
-        quotient /= to_base;
+        decimal_num /= to_base;
     }
+
+    // add null character to the end of the string
     converted_num[temp] = '\0';
 
     // reverse the converted_num string
-    for (i = 0, j = temp - 1; i < j; i++, j--) {
+    for (int i = 0, j = temp - 1; i < j; i++, j--) {
         char temp = converted_num[i];
         converted_num[i] = converted_num[j];
         converted_num[j] = temp;
     }
+
     return converted_num;
 }
+
--- base converter/src/main.c
+++ base converter/src/main.c
@@ -1,49 +1,98 @@
 #include <stdio.h>
+#include <string.h>
+#include<math.h>
 #include "convert_base_file.c"
-
 int main()
 {
-    char num[100];
-    int from_base, to_base;
+    char repeat = 'y';
+    while (repeat == 'y') {
+        char num[100];
+        int from_base,to_base;
 
-    printf("Enter a number: ");
-    scanf("%s", num);
+        printf("Enter a number: ");
+        scanf("%s", num);
 
-    printf("Enter the base of the number: ");
-    scanf("%d", &from_base);
 
-    printf("Select the base to convert to:\n");
-    printf("1. Binary\n");
-    printf("2. Octal\n");
-    printf("3. Decimal\n");
-    printf("4. Hexadecimal\n");
-    printf("Enter your choice: ");
+        printf("Enter the base of the number: ");
+        scanf("%d", &from_base);
 
-    scanf("%d", &to_base);
+        const char* from_base_string = "";
+        switch(from_base) {
+            case 2:
+                from_base_string = "binary";
+                break;
+            case 8:
+                from_base_string = "octal";
+                break;
+            case 10:
+                from_base_string = "decimal";
+                break;
+            case 16:
+                from_base_string = "hexdecimal" ;
+                break;
+            default:
+                printf("Invalid choice\n");
+                return 0;
+        }
 
-    switch(to_base) {
-        case 1:
-            to_base = 2;
-            break;
-        case 2:
-            to_base = 8;
-            break;
-        case 3:
-            to_base = 10;
-            break;
-        case 4:
-            to_base = 16;
-            break;
-        default:
-            printf("Invalid choice\n");
-            return 0;
+        printf("Select the base to convert to:\n");
+        printf("1. Binary\n");
+        printf("2. Octal\n");
+        printf("3. Decimal\n");
+        printf("4. Hexadecimal\n");
+        printf("5. Quit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &to_base);
+
+        const char* to_base_string = "";
+        switch(to_base) {
+            case 1:
+                to_base = 2;
+                to_base_string = "binary";
+                break;
+            case 2:
+                to_base = 8;
+                to_base_string = "octal";
+                break;
+            case 3:
+                to_base = 10;
+                to_base_string = "decimal";
+                break;
+            case 4:
+                to_base = 16;
+                to_base_string = "hexdecimal" ;
+                break;
+            case 5:
+                printf("Program terminated\n");
+                return 0;
+            default:
+                printf("Invalid choice\n");
+                return 0;
+        }
+        char *converted_num = convert_base(num, from_base, to_base);
+        if (converted_num) 
+        {
+        printf("-----------------------\n");
+        printf("\
+            |From the {%s} base.|\n\
+            |Converted to the {%s} base.|\n\
+            |The Converted number is :{%s}|\n"
+            ,from_base_string,to_base_string,converted_num);
+        printf("-----------------------\n");
+
+        } else {
+            printf("Invalid number or base\n");
+        }
+
+        printf("Would you like to repeat the program? (y/n) ");
+        scanf(" %c", &repeat);
+       if (repeat == 'n')
+    {
+        printf("goodbye");
+        return 0;
     }
-
-    char *converted_num = convert_base(num, from_base, to_base);
-    if (converted_num) {
-        printf("Converted number: %s\n", converted_num);
-    } else {
-        printf("Invalid number or base\n");
+        
     }
     return 0;
 }
+    
