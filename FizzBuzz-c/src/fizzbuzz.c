6e207f0bfd86cb27ae4f36545a3ce2001295441e
Salah-Eddine ElOuali <50548046+akasheidheda@users.noreply.github.com>
2023-01-26 14:18:35 +0100

    Add files via upload

---
+++ FizzBuzz-c/src/fizzbuzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+main()
+{
+    int i;
+    for (i = 1; i < 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("fizzbuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("buzz\n");
+        }
+        else 
+        {
+            printf("%i\n",i);
+        }
+    }
+}
