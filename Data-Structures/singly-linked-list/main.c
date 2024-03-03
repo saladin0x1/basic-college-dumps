e413d2be5bbd8220dc92a5ac9cafd3cdfa82d9df
SaladinX <Salaheddine.elouali@emsi-edu.ma>
2024-03-03 18:22:34 +0100

    some mods

--- Data-Structures/singly-linked-list/main.c
+++ Data-Structures/singly-linked-list/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Define a node structure for a linked list
 struct node {
-    int value;
-    struct node *next;
+    int value;           // Data of the node
+    struct node *next;   // Pointer to the next node in the list
 };
 
-struct node *tete;
+struct node *tete;  // Head pointer of the linked list
 
+// Function prototypes
 void init();
 int testVide();
 void AjouterPr(int element);
@@ -15,32 +17,34 @@ void AjouterDr(int element);
 void afficher();
 void libererMemoire();
 
-int main()
-{
+int main() {
+    // Initialize the linked list
     init();
 
     int option, element;
-    while (1)
-    {
+    while (1) {
         //system("clear");
+        // Display menu options
         printf("Veuillez Choisir une Option:\n 1) Ajouter Une nouvelle cellule en tete de la liste. \n 2) Ajouter des cellules a la fin de la liste \n 3) Afficher La liste \n => ");
         scanf("%d", &option);
 
-        switch (option)
-        {
+        switch (option) {
             case 1:
                 system("clear");
+                // Add a new node at the beginning of the list
                 printf("Saisir l'element a ajouter au premier de la liste: ");
                 scanf("%d", &element);
                 AjouterPr(element);
                 break;
             case 2:
                 system("clear");
+                // Add a new node at the end of the list
                 printf("Saisir l'element a ajouter a la fin de la liste: ");
                 scanf("%d", &element);
                 AjouterDr(element);
                 break;
             case 3:
+                // Display the linked list
                 afficher();
                 break;
             default:
@@ -51,23 +55,22 @@ int main()
     return 0;
 }
 
-void init()
-{
+// Initialize the linked list
+void init() {
     tete = NULL;
 }
 
-int testVide()
-{
-    if (tete == NULL)
-    {
+// Check if the linked list is empty
+int testVide() {
+    if (tete == NULL) {
         printf("la liste est vide. \n");
         return 1;
     }
     return 0;
 }
 
-void AjouterPr(int element)
-{
+// Add a new node at the beginning of the list
+void AjouterPr(int element) {
     struct node *newNode;
     newNode = (struct node *)malloc(sizeof(struct node));
     if (newNode == NULL) {
@@ -79,8 +82,8 @@ void AjouterPr(int element)
     tete = newNode;
 }
 
-void AjouterDr(int element)
-{
+// Add a new node at the end of the list
+void AjouterDr(int element) {
     struct node *newNode, *tmp;
     newNode = (struct node *)malloc(sizeof(struct node));
     if (newNode == NULL) {
@@ -90,12 +93,9 @@ void AjouterDr(int element)
     newNode->value = element;
     newNode->next = NULL;
     tmp = tete;
-    if (tmp == NULL)
-    {
+    if (tmp == NULL) {
         tete = newNode;
-    }
-    else
-    {
+    } else {
         while (tmp->next != NULL)
             tmp = tmp->next;
         tmp->next = newNode;
@@ -103,14 +103,13 @@ void AjouterDr(int element)
     printf("element inséré\n");
 }
 
-void afficher()
-{
+// Display the linked list
+void afficher() {
     if (testVide())
         return;
 
     struct node *current = tete;
-    while (current != NULL)
-    {
+    while (current != NULL) {
         //system("clear");
         printf("%d->", current->value);
         current = current->next;
@@ -118,13 +117,12 @@ void afficher()
     printf("NULL\n");
 }
 
-void libererMemoire()
-{
+// Free the memory allocated for the linked list
+void libererMemoire() {
     struct node *current = tete;
     struct node *next;
 
-    while (current != NULL)
-    {
+    while (current != NULL) {
         next = current->next;
         free(current);
         current = next;
